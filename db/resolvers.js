ef7d7d0d39a22be26802a98b9fe77582a7e24e04
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-15 16:55:15 -0300

    Moviendo Type Definitions y resolvers hacia su propio archivo

---
+++ db/resolvers.js
@@ -0,0 +1,30 @@
+const cursos =[
+    {
+        titulo :" JavaScript Moderno Guia Definitiva Construye +10 proyectos",
+        tecnologia : "JavaScript ES6"
+    },
+    {
+        titulo :" React- La Guia Completa : Hooks Context Redux MERN + 15 Apps",
+        tecnologia : "React"
+    },
+    {
+        titulo :" Node.js - Bootcamp Desarrollo Web inc. MVC y REST API´s",
+        tecnologia : "Node.js"
+    },
+    {
+        titulo :"ReactJS Avanzado - FullStack React GraphQL y Apollo",
+        tecnologia : "React"
+    },
+    
+]
+
+
+const resolvers ={
+    Query : {
+        obtenerCuros : ()=> cursos,
+
+        obtenerTecnologia : ()=>  cursos
+    }
+}
+
+module.exports=resolvers
--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-const {ApolloServer, gql}= require("apollo-server")
+const {ApolloServer}= require("apollo-server")
 
-const typeDefs = gql`
-
-    type Curso {
-        titulo : String
-        tecnologia : String
-    }
-    type Query {
-        obtenerCuros : [Curso]
-    }
-`;
-
-const cursos =[
-    {
-        titulo :" JavaScript Moderno Guia Definitiva Construye +10 proyectos",
-        tecnologia : "JavaScript ES6"
-    },
-    {
-        titulo :" React- La Guia Completa : Hooks Context Redux MERN + 15 Apps",
-        tecnologia : "React"
-    },
-    {
-        titulo :" Node.js - Bootcamp Desarrollo Web inc. MVC y REST API´s",
-        tecnologia : "Node.js"
-    },
-    {
-        titulo :"ReactJS Avanzado - FullStack React GraphQL y Apollo",
-        tecnologia : "React"
-    },
-    
-]
+const typeDefs = require("./db/schema")
+const resolvers = require("./db/resolvers")
 
 
-const resolvers ={
-    Query : {
-        obtenerCuros : ()=> cursos
-    }
-}
+
 
 const server = new ApolloServer({typeDefs,resolvers})
 
