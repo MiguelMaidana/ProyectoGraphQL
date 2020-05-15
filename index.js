0ce6057e77d2312df191a899b430e64bac8aad5a
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-15 16:21:21 -0300

    GraphQL type Definitions, Creando un Query y un Resolver. Arranca mi Server, Grapycal

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const {ApolloServer, gql}= require("apollo-server")
 
+const typeDefs = gql`
 
-const server = new ApolloServer()
+    type Curso {
+        titulo : String
+        tecnologia : String
+    }
+    type Query {
+        obtenerCuros : [Curso]
+    }
+`;
+
+const cursos =[
+    {
+        titulo :" JavaScript Moderno Guia Definitiva Construye +10 proyectos",
+        tecnologia : "JavaScript ES6"
+    },
+    {
+        titulo :" React- La Guia Completa : Hooks Context Redux MERN + 15 Apps",
+        tecnologia : "React"
+    },
+    {
+        titulo :" Node.js - Bootcamp Desarrollo Web inc. MVC y REST API´s",
+        tecnologia : "Node.js"
+    },
+    {
+        titulo :"ReactJS Avanzado - FullStack React GraphQL y Apollo",
+        tecnologia : "React"
+    },
+    
+]
+
+
+const resolvers ={
+    Query : {
+        obtenerCuros : ()=> cursos
+    }
+}
+
+const server = new ApolloServer({typeDefs,resolvers})
 
 server.listen().then(({url})=>{
     console.log(`Servidor listo en la URL ${url}`)
