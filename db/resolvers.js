f05827beb3c43cdb11db39a01aebff9b96c94f96
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-17 15:21:29 -0300

    Autenticacion de Usuarios

--- db/resolvers.js
+++ db/resolvers.js
@@ -21,11 +21,11 @@ const resolvers ={
             const salt = await bcrypts.genSalt(10)
             input.password = await bcrypts.hash(password, salt)
 
-            console.log(input)
+            //console.log(input)
 
             // registrart nuevo usuario 
             const nuevoUsuario = new Usuario(input)
-            console.log(nuevoUsuario)
+            //console.log(nuevoUsuario)
 
             // guardo En la base de datos 
             nuevoUsuario.save()
@@ -36,6 +36,30 @@ const resolvers ={
           catch(error){
               console.log(error)
           }
+    },
+    autenticarUsuario : async (_,{input}) =>{
+        const {email,password} = input
+
+        // revisar si el usuario existe
+
+        const existeUsuario = await Usuario.findOne({email})
+
+        // Si el Usuario Ya existe
+          if(!existeUsuario){
+              throw new Error ("El usuario No existe ")
+          }
+
+        // si el password es correcto
+
+        const passwordCorrecto = await bcrypts.compare(password,existeUsuario.password)
+
+        if(!passwordCorrecto){
+            throw new Error("Password Incorrecto")
+        }
+
+        // Dar acceso a la app
+
+        return "Has Iniciado Sesion"
     }
     }
 }
